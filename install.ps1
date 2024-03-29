2dfc7bdc438ee23e251762f583b3b370929798b4
LaCapitainerie <hugo.antreassian>
2024-03-29 12:16:12 +0100

    SQL working

--- install.ps1
+++ install.ps1
@@ -1,1 +1,1 @@
-cat createBDD.sql | ./sqlite/sqlite3.exe
+Write-Output '{"mails":[],"users":[]}' > PINGUMAIL.json
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	
+
 	fmt.Println("Launching server...")
 
 	server.Start()
